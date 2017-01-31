96c2952b1836c14426d0156d271a744c8701e1d3
GearShift24 <gearshift24@gmail.com>
2017-01-31 09:29:20 -0700

    added timer model

---
+++ secondProject/Model/Timer.cpp
@@ -0,0 +1,43 @@
+//
+//  Timer.cpp
+//  secondProject
+//
+//  Created by Wrigley, Joseph on 1/31/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include "Timer.hpp"
+#include <iostream>
+
+using namespace std;
+
+Timer :: Timer()
+{
+    executionTime = 0;
+}
+
+void Timer :: resetTimer()
+{
+    executionTime = 0;
+}
+
+void Timer :: startTimer()
+{
+    executionTime = clock();
+}
+
+void Timer :: stopTimer()
+{
+    executionTime = clock() - executionTime;
+}
+
+void Timer :: displayTimerInformation()
+{
+    cout << "The amount of computed time is: " << executionTime << endl;
+    cout <<"In human time it is " << executionTime/CLOCKS_PER_SEC << " seconds" << endl;
+}
+long Timer :: getExecutionTimeInMicroseconds()
+{
+    return executionTime;
+}
+
